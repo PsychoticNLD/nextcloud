8413de469a28884f4739fc33e849d3eb60235efe
PsychoticNLD <149720906+PsychoticNLD@users.noreply.github.com>
2025-05-19 21:54:08 +0200

    Update install_nextcloud.sh
    php8.2-smbclient

--- install_nextcloud.sh
+++ install_nextcloud.sh
@@ -45,7 +45,7 @@ install_nextcloud() {
     sudo apt install apache2 mariadb-server software-properties-common unzip -y || { echo "Failed to install necessary packages"; exit 1; }
     sudo add-apt-repository ppa:ondrej/php -y || { echo "Failed to add PHP repository"; exit 1; }
     sudo apt update || { echo "Failed to update package list"; exit 1; }
-    sudo apt install php8.2 libapache2-mod-php8.2 php8.2-gd php8.2-mysql php8.2-curl php8.2-mbstring php8.2-intl php8.2-imagick php8.2-xml php8.2-zip php8.2-opcache php8.2-redis redis-server -y || { echo "Failed to install PHP packages"; exit 1; }
+    sudo apt install php8.2 libapache2-mod-php8.2 php8.2-gd php8.2-mysql php8.2-curl php8.2-mbstring php8.2-intl php8.2-imagick php8.2-xml php8.2-zip php8.2-opcache php8.2-redis redis-server php8.2-smbclient -y || { echo "Failed to install PHP packages"; exit 1; }
 
     echo "Starting and securing MariaDB..."
     sudo systemctl start mariadb || { echo "Failed to start MariaDB"; exit 1; }
